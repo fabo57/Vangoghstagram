2d5c2f3cb58e3346e169e586fe9aec6c332dbd1f
Fabian Magallanes <fabi.maga57@gmail.com>
2017-06-27 19:14:20 -0500

    Ya corre

--- vangoghstagram.py
+++ vangoghstagram.py
@@ -5,5 +5,9 @@ from flask import Flask, render_template, jsonify
 app = Flask(__name__)
 
 @app.route("/")
-    def hola_mundo():
+def hola_mundo():
         return "Hola a todos"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
